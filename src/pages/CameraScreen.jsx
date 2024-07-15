be21da963c3b679df9b6bc94ee54a567d1afd966
Firalen <140722327+Firalen@users.noreply.github.com>
2024-07-15 19:45:37 +0300

    Update CameraScreen.jsx

--- src/pages/CameraScreen.jsx
+++ src/pages/CameraScreen.jsx
@@ -1,13 +1,101 @@
- 
-import React, { useEffect, useState, useRef } from 'react';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-const CameraScreen = ({ navigation }) => {
- 
+export default function App() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef(null);
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
-        
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
   }
 
-export default CameraScreen;
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  async function takePicture() {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo.uri);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+      <View style={styles.textContainer}>
+        <TextInput
+          style={styles.textArea}
+          multiline={true}
+          numberOfLines={10}
+          placeholder="Type here..."
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1, // Reduced flex value for the camera
+    height: 300,
+    width: 300,
+    marginLeft: 50,
+    marginTop: 20,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  textArea: {
+    height: 300,
+    width: 300,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
+    textAlignVertical: 'top',
+  },
+  textContainer: {
+    flex: 1, // Added flex value for the text area
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
